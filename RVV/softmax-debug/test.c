164cb3f2143e24137779095804a6aaeced281b11
zxlcreate <18519889511@163.com>
2025-09-25 23:03:20 +0800

    softmax debug
    change way of input args in softmax function

--- RVV/softmax-debug/test.c
+++ RVV/softmax-debug/test.c
@@ -106,9 +106,8 @@ uint32_t quick_dirty_vector_expf_no_scalar(float* dst, float* src, uint32_t max_
 
 int main(){
     //softmax_stable_rvv_fp32(dst,src,VLEN/32);
-    int a =1;
-    int b =1;
-    int c = a + b;
+    softmax_stable_rvv_fp32(dst,src,N);
+
     //softmax_stable_rvv_fp32(dst, (float*)src, N);
     return 0;
 }
